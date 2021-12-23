7fb784e634133684a2995227e1a16021788a2595
B1acksmith <b1ack7mit4@gmail.com>
2021-12-23 16:10:14 +0300

    main

---
+++ gulp/config/path.js
@@ -0,0 +1,21 @@
+// Получаем имя папки нашего проекта
+import * as nodePath from 'path';
+import { build } from '../../gulpfile';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const project_folder = `./dist`;   // Также можно использовать rootFolder
+const source_folder = `./#src`;
+
+export const path = {
+    build: {
+        files: `${buildFolder/files/`
+    },
+    src: {
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {},
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ``
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+// Основной модуль
+import gulp from "gulp";
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+
+// Передаем значения в глобальную переменную
+global.app = {
+    path: path,
+    gulp: gulp
+}
+
+
+
+// let project_folder = "dist";
+// let source_folder = "#src";
+
+// let path = {
+//     build:{
+//         html: project_folder + "/",
+//         css: project_folder + "/css/",
+//         js: project_folder + "/js/",
+//         img: project_folder + "/img/",
+//         fonts: project_folder + "/fonts/",
+//     },
+//     src:{
+//         html: source_folder + "/*html",
+//         css: source_folder + "/scss/style.scss",
+//         js: source_folder + "/js/script.js",
+//         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+//         fonts: source_folder + "/fonts/*.ttf",
+//     },
+//     watch:{
+//         html: source_folder + "/**/*.html",
+//         css: source_folder + "/scss/**/*.scss",
+//         js: source_folder + "/js/**/*.js",
+//         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+//     },
+//     claen:"./" + project_folder + "/"
+// }
+
+// let { src, dest } = require ('gulp'),
+//     gulp = require ('gulp'),
+//     browsersync = require ('browser-sync').create();
+
+// function browserSync(params) {
+//     browsersync.init({
+//         server:{
+//             baseDir: "./" + project_folder + "/"
+//         },
+//         port: 3000,
+//         notify: false
+//     })
+// }
+
+// function html () {
+//     return src(path.src.html)
+//         .pipe(dest(path.build.html))
+//         .pipe(browsersync.stream())
+// }
+
+// let build = gulp.series(html);
+// let watch = gulp.parallelbuild(build, browserSync);
+
+// exports.html = html;
+// exports.build = build;
+// exports.watch = watch;
+// exports.default = watch;
