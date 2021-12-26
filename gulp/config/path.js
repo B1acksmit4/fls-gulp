4458bcfcd13b4025937435cd9b4b227f454f97ce
B1acksmith <b1ack7mit4@gmail.com>
2021-12-26 13:26:08 +0300

    main

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,6 +1,6 @@
 // Получаем имя папки нашего проекта
 import * as nodePath from 'path';
-import { build } from '../../gulpfile';
+// import { build } from '../../gulpfile';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
 const project_folder = `./dist`;   // Также можно использовать rootFolder
@@ -8,7 +8,7 @@ const source_folder = `./#src`;
 
 export const path = {
     build: {
-        files: `${buildFolder}/**/*.*`
+        files: `${buildFolder}/files/`
     },
 
     src: {
